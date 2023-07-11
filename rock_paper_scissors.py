316ea66a1e89eedbb65036be499da97e460b8e3c
Ryan <zhangryan1357@gmail.com>
2023-07-11 22:44:46 +0100

    dictionary added

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,18 +1,33 @@
 import random
 
+
 #Input user decision
-choice = input("Choose 1, 2, 3, rock paper sissors respectively ")
-if (choice != "1" and choice != "2" and choice != "3"):
-    print("Input invalid")
+choice = input("Choose Rock, Paper or Scissors respectively > ")
+
+optiondict = {
+    "Rock":  "1",
+    "Paper": "2",
+    "Scissors": "3"
+}
+
+if (choice != "Rock" and choice != "Paper" and choice != "Scissors" and choice != "rock" and choice != "paper" and choice != "scissors"):
+    print("Input is invalid, please select either Rock, Paper, Scissors")
     exit()
 else:
-    choice = int(choice)
+    choice = int(optiondict[choice])
 
 #Generate computer decision
-opchoice = (random.randint(1, 3)) 
+opchoice = str((random.randint(1, 3)) )
 
+#reverses dictionary 
+print("Your opponent played", list(optiondict.keys())
+      [list(optiondict.values()).index(opchoice)])
+
+opchoice = int(opchoice)
+
+#calculates who won
 if choice - opchoice == 0:
-    print("Draw! The same choice!")#
+    print("Draw! The same choice!")
 elif choice - opchoice == 1 or choice - opchoice == -2:
     print("You win")
 else:
